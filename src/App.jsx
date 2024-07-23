b54bda9dcfef6ab9514322a6c94c00f36e0df06e
Bedinah <bernabazubagira@gmail.com>
2024-07-23 11:49:51 +0200

    initial commits

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import Navbar from "./components/NavBar";
+import Hero from "./components/Hero";
+import HomeCards from "./components/HomeCards";
+import JobListings from "./components/JobListings";
+import ViewAllJobs from './components/ViewAllJobs';
+
+const App = () => {
+  return (
+  <>
+ <Navbar/>
+ <Hero/>
+ <HomeCards/>
+ <JobListings/>
+ <ViewAllJobs/>
+  </>
+  );
+};
+
+export default App;
